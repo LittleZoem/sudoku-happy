f6e2218f9726f4b89fd8022a9dc41fcc66ab0e0b
fangzheng <2413606723@qq.com>
2025-01-04 21:38:28 +0800

    策略测试

--- src/components/Controls/ActionBar/Actions.svelte
+++ src/components/Controls/ActionBar/Actions.svelte
@@ -8,19 +8,29 @@
 	import { keyboardDisabled } from '@sudoku/stores/keyboard';
 	import { gamePaused } from '@sudoku/stores/game';
 	import { roam } from '@sudoku/stores/roam';
-	import { strategyManeger } from '@sudoku/strategy';
+	import { strategyManager } from '@sudoku/strategy/strategy_manager';
+	import { get } from 'svelte/store';
 
-	$: hintsAvailable = $hints > 0;
+	// $: hintsAvailable = $hints > 0;
 
 	function handleHint() {
-		if (hintsAvailable) {
-			if ($candidates.hasOwnProperty($cursor.x + ',' + $cursor.y)) {
-				candidates.clear($cursor);
-			}
-			
-			userGrid.applyHint($cursor);
-			strategyManeger.run(userGrid);
-		}
+		// if (hintsAvailable) {
+		// if ($candidates.hasOwnProperty($cursor.x + ',' + $cursor.y)) {
+		// 	candidates.clear($cursor);
+		// }
+
+		let grid = get(userGrid);
+
+		console.log(grid);
+
+		strategyManager.run(grid);
+
+		cursor.reset();
+
+		userGrid.showHint(strategyManager.getHint());
+
+		console.log(strategyManager.getHint());
+		// }
 	}
 </script>
 
@@ -50,14 +60,14 @@
 		</svg>
 	</button>
 
-	<button class="btn btn-round btn-badge" disabled={$keyboardDisabled || !hintsAvailable || $userGrid[$cursor.y][$cursor.x] !== 0} on:click={handleHint} title="Hints ({$hints})">
+	<button class="btn btn-round btn-badge" disabled = {false} on:click={handleHint} title="Hints ({$hints})">
 		<svg class="icon-outline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
 		</svg>
 
-		{#if $settings.hintsLimited}
+		<!-- {#if $settings.hintsLimited}
 			<span class="badge" class:badge-primary={hintsAvailable}>{$hints}</span>
-		{/if}
+		{/if} -->
 	</button>
 
 	<button class="btn btn-round btn-badge" on:click={notes.toggle} title="Notes ({$notes ? 'ON' : 'OFF'})">
